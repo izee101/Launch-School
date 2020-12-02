2430f49b5d28e4a76e1a8cb89da2310326a247d3
Isaiah Simon <isaiahsimon101@gmail.com>
2020-12-02 09:27:16 -0500

    refractor RPS game

--- rb101/lesson_2/rock_paper_scissors.rb
+++ rb101/lesson_2/rock_paper_scissors.rb
@@ -4,6 +4,20 @@ def prompt(message)
   puts("=> #{message}")
 end
 
+def display_results(player, computer)
+  if (player == 'rock' && computer == 'scissors') ||
+     (player == 'paper' && computer == 'rock') ||
+     (player == 'scissors' && computer == 'paper')
+    prompt("You won!")
+  elsif (player == 'rock' && computer == 'paper') ||
+        (player == 'paper' && computer == 'scissors') ||
+        (player == 'scissors' && computer == 'rock')
+    prompt("Computer won!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
 loop do
   choice = ''
   loop do
@@ -19,19 +33,9 @@ loop do
 
   computer_choice = VALID_CHOICES.sample
 
-  puts("You chose: #{choice}; Computer chose: #{computer_choice}")
+  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
 
-  if (choice == 'rock' && computer_choice == 'scissors') ||
-     (choice == 'paper' && computer_choice == 'rock') ||
-     (choice == 'scissors' && computer_choice == 'paper')
-    prompt("You won!")
-  elsif (choice == 'rock' && computer_choice == 'paper') ||
-        (choice == 'paper' && computer_choice == 'scissors') ||
-        (choice == 'scissors' && computer_choice == 'rock')
-    prompt("Computer won!")
-  else
-    prompt("It's a tie!")
-  end
+  display_results(choice, computer_choice)
 
   prompt("Do you want to play again?")
   answer = gets.chomp
