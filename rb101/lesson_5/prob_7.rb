8584a9da2ad6bcc05ca37ef2edebe7694c0d1bd4
Isaiah Simon <isaiahsimon101@gmail.com>
2021-01-02 22:20:32 -0500

    work on lesson 5 practice problems

--- rb101/lesson_5/prob_7.rb
+++ rb101/lesson_5/prob_7.rb
@@ -2,7 +2,19 @@
 
 a = 2
 b = [5, 8]
-arr = [a ,b]
+arr = [a, b]
 
 arr[0] += 2
-arr[1][0] -+ a
+arr[1][0] -= a
+
+# a = 4
+# b = [3, 8]
+
+=begin
+    The value of a didn't change because we are not referencing a at any point.
+    This code arr[0] += 2 was modifying the array, arr not a.
+    In effect we are assigning a new object at that index of the array so that instead of arr[0]
+    containing a it now contains 4 - we can check this by looking at the value of arr:
+        arr # => [4, [3, 8]]
+    The value of b did change because b is an array and we are modifying that array by assigning a new value at index 0 of that array.
+=end
