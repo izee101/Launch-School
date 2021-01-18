639699cc44fadb15d6679ca22947ec3cf0db5cde
Isaiah Simon <isaiahsimon101@gmail.com>
2021-01-18 14:56:21 -0500

    work on problem

---
+++ Exercises/1.RB101_Small_Problems/4.Easy_4/7.Convert_a_String_to_a_Number.rb
@@ -0,0 +1,52 @@
+=begin
+
+#Problem: Convert a String to a Number!
+  The String#to_i method converts a string of numeric characters (including an optional
+  plus or minus sign) to an Integer. String#to_i and the Integer constructor (Integer())
+  behave similarly. In this exercise, you will create a method that does the same thing.
+
+Write a method that takes a String of digits, and returns the appropriate number as an
+integer. You may not use any of the methods mentioned above.
+
+For now, do not worry about leading + or - signs, nor should you worry about invalid
+  characters; assume all characters will be numeric.
+
+You may not use any of the standard conversion methods available in Ruby to convert a
+string to a number, such as String#to_i, Integer(), etc. Your method should do this the
+old-fashioned way and calculate the result by analyzing the characters in the string.
+
+  Explicit requirements
+    Input:
+      - string
+    Output:
+      - integer
+    Rules:
+      -cannot use String#to_i, Integer(), etc...
+
+  Implicit requirements:
+    -
+
+  Questions:
+    -Invalid characters? = IGNORE
+    -Negative numbers? = IGNORE
+
+#Examples / Test Cases:
+
+  string_to_integer('4321') == 4321
+  string_to_integer('570') == 570
+
+#Data Structures
+  Possible methods:
+    -
+  Possible structures:
+    -
+
+#Algorithm
+  -
+
+   => expected return:
+   => expected output:
+
+=end
+
+#Code Implementation
