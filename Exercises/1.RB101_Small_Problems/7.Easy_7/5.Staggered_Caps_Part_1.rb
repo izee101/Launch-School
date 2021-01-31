82aa347781e09cc6dbdc17d935b23995ea602ebd
Isaiah Simon <isaiahsimon101@gmail.com>
2021-01-31 18:23:36 -0500

    work on problem

---
+++ Exercises/1.RB101_Small_Problems/7.Easy_7/5.Staggered_Caps_Part_1.rb
@@ -0,0 +1,61 @@
+=begin
+
+#Problem: Staggered Caps (Part 1)
+  Write a method that takes a String as an argument, and returns a new String that contains the
+  original value using a staggered capitalization scheme in which every other character is
+  capitalized, and the remaining characters are lowercase. Characters that are not letters
+  should not be changed, but count as characters when switching between upper and lowercase.
+
+  Explicit requirements
+    Input:
+      -string
+    Output:
+      -string
+    Rules:
+      -
+
+  Implicit requirements:
+    -
+
+  Questions:
+    -
+
+#Examples / Test Cases:
+
+  staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
+  staggered_case('ALL_CAPS') == 'AlL_CaPs'
+  staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
+
+#Data Structures
+  Possible methods:
+    -
+  Possible structures:
+    -
+
+#Algorithm
+  -
+
+   => expected return:
+   => expected output:
+
+=end
+
+#Code Implementation
+
+def staggered_case(string)
+  result = ''
+  need_upper = true
+  string.chars.each do |char|
+    if need_upper
+      result += char.upcase
+    else
+      result += char.downcase
+    end
+    need_upper = !need_upper
+  end
+  result
+end
+
+p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
+p staggered_case('ALL_CAPS') == 'AlL_CaPs'
+p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
